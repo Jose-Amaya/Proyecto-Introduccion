639ba3d707e6572bd8bcf81a4a17b2bfcb5f1e63
JOSE AMAYA <jose2170484@correo.uis.edu.co>
2019-12-18 19:43:07 -0500

    Upgrade

--- 2. Programación/project/data/dataTranslate.py
+++ 2. Programación/project/data/dataTranslate.py
@@ -13,44 +13,52 @@ import dataGet as dg
 import time
 
 class Central():
-    def __init__(self, id, name, DN, OI):
+    def __init__(self, id, name, mapName, DN, OI, index):
         self.id = id
         self.name = name
+        self.mapName = mapName
+
         self.DNlist = DN
         self.OIlist = OI
+
         self.DNgen = [0]*24
         self.OIgen = [0]*24
+
         self.DNfound = False
         self.OIfound = False
-        self.ActualGen = 0
+
+        self.currentGen = 0
+
+        self.index = index
+        
     
     def get(self, hour, type = 0): # 0 = DN, other = OI
         if type is 0:
             if self.DNfound:
-                self.ActualGen = self.DNgen[hour]
+                self.currentGen = self.DNgen[hour]
                 return(self.DNgen[hour])
             else:
-                self.ActualGen = self.OIgen[hour]
+                self.currentGen = self.OIgen[hour]
                 return(self.OIgen[hour])
         else:
             if self.OIfound:
-                self.ActualGen = self.OIgen[hour]
+                self.currentGen = self.OIgen[hour]
                 return(self.OIgen[hour])
             else:
-                self.ActualGen = self.DNgen[hour]
+                self.currentGen = self.DNgen[hour]
                 return(Self.DNgen[hour])
 
 
 #Lista de centrales, [ID, NAME, [DN NAME 1, ...], [OI NAME 1, ...]] # about DN NAME and OI NAME, we use regex, so escape any special character
-CentralesRaw = [['E1', 'JEPIRACHI1', ['MJEPIRAC'], ['JEPIRACHI1-15']], ['T1', 'FLORES', ['FLORES IV', 'FLORES1'], ['FLORES1', 'FLORES4B']], ['T2', 'BARRANQUILLA', ['TEBSA'], ['BARRANQUILLA3', 'BARRANQUILLA4', 'TEBSAB']], ['T3', 'GUAJIRA', ['GUAJIR11', 'GUAJIR21'], ['GUAJIRA1', 'GUAJIRA2']], ['T4', 'TASAJERO', ['TASAJER1', 'TASAJERO2'], ['TASAJERO1', 'TASAJERO2']], ['T12', 'TERMOSIERRAB', ['TSIERRA'], ['TERMOSIERRAB']], ['T6', 'TERMOEMCALI1', ['TEMCALI'], ['TERMOEMCALI1']], ['T7', 'PAIPA', ['PAIPA1', 'PAIPA2', 'PAIPA3', 'PAIPA4'], ['PAIPA1', 'PAIPA2', 'PAIPA3', 'PAIPA4']], ['T8', 'TERMOCENTROCC', ['TCENTRO1'], ['TERMOCENTROCC']], ['T9', 'ZIPAEMG', ['ZIPAEMG2', 'ZIPAEMG3', 'ZIPAEMG4', 'ZIPAEMG5'], ['ZIPAEMG2', 'ZIPAEMG3', 'ZIPAEMG4', 'ZIPAEMG5']], ['T10', 'CARTAGENA', ['CTGEMG1', 'CTGEMG2', 'CTGEMG3'], ['CARTAGENA1', 'CARTAGENA2', 'CARTAGENA3']], ['T11', 'MERILECTRICA1', ['MERILEC1'], ['MERILECTRICA1']], ['H1', 'LAHERRADURA', ['LAHERRADURA'], ['LAHERRADURA1', 'LAHERRADURA2']], ['H2', 'JAGUAS', ['JAGUAS'], ['JAGUAS1', 'JAGUAS2']], ['H3', 'SANCARLOS', ['CSANCARLOS'], ['SANCARLOS1', 'SANCARLOS2', 'SANCARLOS3', 'SANCARLOS4', 'SANCARLOS5', 'SANCARLOS6', 'SANCARLOS7', 'SANCARLOS8']], ['H4', 'SOGAMOSO', ['SOGAMOSO'], ['SOGAMOSO1', 'SOGAMOSO2', 'SOGAMOSO3']], ['H5', 'AMOYALAESPERANZA', ['AMOYA'], ['AMOYALAESPERANZA1', 'AMOYALAESPERANZA2']], ['H6', 'MIELI', ['MIEL1'], ['MIELI1', 'MIELI2', 'MIELI3']], ['H7', 'CALDERAS', ['MCALDERAS'], ['CALDERAS1', 'CALDERAS2']], ['H8', 'GUAVIO', ['GUAVIO', 'MGUAVIO'], ['GUAVIO1', 'GUAVIO2', 'GUAVIO3', 'GUAVIO4', 'GUAVIO5']], ['H9', 'BETANIA', ['BETANIA'], ['BETANIA1', 'BETANIA2', 'BETANIA3']], ['H10', 'ELQUIMBO', ['ELQUIMBO'], ['ELQUIMBO1', 'ELQUIMBO2']], ['H11', 'PARAISO', ['PAGUA'], ['PARAISO1', 'PARAISO2', 'PARAISO3']], ['H12', 'LAGUACA', ['PAGUA'], ['LAGUACA1', 'LAGUACA2', 'LAGUACA3']], ['H13', 'TEQUENDAMA', ['MB_TEQUENDA','MTEQUEND1','MTEQUEND2','MTEQUEND3','MTEQUEND4'], ['TEQUENDAMA1','TEQUENDAMA2','TEQUENDAMA3','TEQUENDAMA4']], ['H14', 'LATASAJERA', ['LATASAJERA'], ['LATASAJERA1', 'LATASAJERA2', 'LATASAJERA3']], ['H15', 'CARACOLI', ['MCARACOLI'], ['CARACOLI']], ['H16', 'ELLIMONAR', ['MLIMONAR'], ['ELLIMONAR']], ['H17', 'DARIOVALENCIASAMPER', ['DARIOVS'], ['DARIOVALENCIASAMPER1', 'DARIOVALENCIASAMPER2', 'DARIOVALENCIASAMPER5']], ['H18', 'LAGUNETA', ['MLAGUNETA'], ['LAGUNETA']], ['H19', 'PAJARITO', ['MPAJARITO'], ['PAJARITO1','PAJARITO2']], ['H20', 'SANJOSEDELAMONTANA', ['MSNJOSE_MONT'], ['SANJOSEDELAMONTAÑA','SANJOSEDELAMONTAÑAII1']], ['H1', 'TRONERAS', ['GUATRON'], ['TRONERAS1', 'TRONERAS2']], ['H2', 'GUADALUPE3', [], ['GUADALUPE31', 'GUADALUPE32', 'GUADALUPE33', 'GUADALUPE34', 'GUADALUPE35', 'GUADALUPE36']], ['H3', 'GUADALUPE4', [], ['GUADALUPE41', 'GUADALUPE42', 'GUADALUPE43']], ['H4', 'PORCEII', ['PORCE2'], ['PORCEII1', 'PORCEII2', 'PORCEII3']], ['H5', 'PORCEIII', ['PORCE3'], ['PORCEIII4', 'PORCEIII3', 'PORCEIII2', 'PORCEIII1']], ['H6', 'GUATAPE', ['GUATAPE'], ['GUATAPE1', 'GUATAPE2', 'GUATAPE3', 'GUATAPE4', 'GUATAPE5', 'GUATAPE6', 'GUATAPE7', 'GUATAPE8']], ['H7', 'RIOABAJO', [], []], ['H8', 'SONSON', ['MSONSON'], ['SONSON']], ['H9', 'RIOFRIO(TAMESIS)', ['MRFRIOTAMES','MRIOFRIO1','RIOFRIO2'], ['RIOFRIO\(TAMESIS\)']], ['H10', 'AYURA', ['M_AYURA'], ['AYURA']], ['H11', 'NIQUIA', ['MNIQUIA'], ['NIQUIA']], ['H12', 'URRA1', ['URRA'], ['URRA1']], ['H13', 'CHIVOR', ['CHIVOR'], ['CHIVOR1', 'CHIVOR2', 'CHIVOR3', 'CHIVOR4', 'CHIVOR5', 'CHIVOR6', 'CHIVOR7', 'CHIVOR8']], ['H14', 'CUCUANA', ['CUCUANA'], ['CUCUANA1', 'CUCUANA2']], ['H15', 'CALIMA', ['CALIMA1'], ['CALIMA1', 'CALIMA2', 'CALIMA3', 'CALIMA4']]]
+CentralesRaw = [['E1', 'JEPIRACHI1', 'Parque Eolico De Jepirachi', ['MJEPIRAC'], ['JEPIRACHI1-15']], ['T1', 'FLORES', 'Termoflores', ['FLORES IV', 'FLORES1'], ['FLORES1', 'FLORES4B']], ['T2', 'BARRANQUILLA', 'Termobarranquilla', ['TEBSA'], ['BARRANQUILLA3', 'BARRANQUILLA4', 'TEBSAB']], ['T3', 'GUAJIRA', 'Termoguajira', ['GUAJIR11', 'GUAJIR21'], ['GUAJIRA1', 'GUAJIRA2']], ['T4', 'TASAJERO', 'Termotasajero', ['TASAJER1', 'TASAJERO2'], ['TASAJERO1', 'TASAJERO2']], ['T12', 'TERMOSIERRAB', 'Termosierra', ['TSIERRA'], ['TERMOSIERRAB']], ['T6', 'TERMOEMCALI1', 'Termoemcali', ['TEMCALI'], ['TERMOEMCALI1']], ['T7', 'PAIPA', 'Termopaipa', ['PAIPA1', 'PAIPA2', 'PAIPA3', 'PAIPA4'], ['PAIPA1', 'PAIPA2', 'PAIPA3', 'PAIPA4']], ['T8', 'TERMOCENTROCC', 'Termocentro', ['TCENTRO1'], ['TERMOCENTROCC']], ['T9', 'ZIPAEMG', 'Termoelectrica Martin Del Corral', ['ZIPAEMG2', 'ZIPAEMG3', 'ZIPAEMG4', 'ZIPAEMG5'], ['ZIPAEMG2', 'ZIPAEMG3', 'ZIPAEMG4', 'ZIPAEMG5']], ['T10', 'CARTAGENA', 'Central Termica De Cartagena', ['CTGEMG1', 'CTGEMG2', 'CTGEMG3'], ['CARTAGENA1', 'CARTAGENA2', 'CARTAGENA3']], ['T11', 'MERILECTRICA1', 'Central Merilectric', ['MERILEC1'], ['MERILECTRICA1']], ['H1', 'LAHERRADURA', 'Central La Herradura', ['LAHERRADURA'], ['LAHERRADURA1', 'LAHERRADURA2']], ['H2', 'JAGUAS', 'Central Jaguas', ['JAGUAS'], ['JAGUAS1', 'JAGUAS2']], ['H3', 'SANCARLOS', 'Central San Carlos', ['CSANCARLOS'], ['SANCARLOS1', 'SANCARLOS2', 'SANCARLOS3', 'SANCARLOS4', 'SANCARLOS5', 'SANCARLOS6', 'SANCARLOS7', 'SANCARLOS8']], ['H4', 'SOGAMOSO', 'Hidrosogamoso', ['SOGAMOSO'], ['SOGAMOSO1', 'SOGAMOSO2', 'SOGAMOSO3']], ['H5', 'AMOYALAESPERANZA', 'Amoya La Esperanza', ['AMOYA'], ['AMOYALAESPERANZA1', 'AMOYALAESPERANZA2']], ['H6', 'MIELI', 'Miel I', ['MIEL1'], ['MIELI1', 'MIELI2', 'MIELI3']], ['H7', 'CALDERAS', 'Calderas', ['MCALDERAS'], ['CALDERAS1', 'CALDERAS2']], ['H8', 'GUAVIO', 'Guavio', ['GUAVIO', 'MGUAVIO'], ['GUAVIO1', 'GUAVIO2', 'GUAVIO3', 'GUAVIO4', 'GUAVIO5']], ['H9', 'BETANIA', 'Represa Betania', ['BETANIA'], ['BETANIA1', 'BETANIA2', 'BETANIA3']], ['H10', 'ELQUIMBO', 'El Quimbo', ['ELQUIMBO'], ['ELQUIMBO1', 'ELQUIMBO2']], ['H11', 'PARAISO', 'Paraiso', ['PAGUA'], ['PARAISO1', 'PARAISO2', 'PARAISO3']], ['H12', 'LAGUACA', 'Guaca', ['PAGUA'], ['LAGUACA1', 'LAGUACA2', 'LAGUACA3']], ['H13', 'TEQUENDAMA', 'Tequendama', ['MB_TEQUENDA', 'MTEQUEND1', 'MTEQUEND2', 'MTEQUEND3', 'MTEQUEND4'], ['TEQUENDAMA1', 'TEQUENDAMA2', 'TEQUENDAMA3', 'TEQUENDAMA4']], ['H14', 'LATASAJERA', 'La Tasajera', ['LATASAJERA'], ['LATASAJERA1', 'LATASAJERA2', 'LATASAJERA3']], ['H15', 'CARACOLI', 'Caracoli', ['MCARACOLI'], ['CARACOLI']], ['H16', 'ELLIMONAR', 'Limonar', ['MLIMONAR'], ['ELLIMONAR']], ['H17', 'DARIOVALENCIASAMPER', 'Dario Valencia', ['DARIOVS'], ['DARIOVALENCIASAMPER1', 'DARIOVALENCIASAMPER2', 'DARIOVALENCIASAMPER5']], ['H18', 'LAGUNETA', 'Laguneta', ['MLAGUNETA'], ['LAGUNETA']], ['H19', 'PAJARITO', 'Minicentral Pajarito', ['MPAJARITO'], ['PAJARITO1', 'PAJARITO2']], ['H20', 'SANJOSEDELAMONTANA', 'Mincentral Central San Jose De La Montaña', ['MSNJOSE_MONT'], ['SANJOSEDELAMONTAÑA', 'SANJOSEDELAMONTAÑAII1']], ['H1', 'TRONERAS', 'Troneras', ['GUATRON'], ['TRONERAS1', 'TRONERAS2']], ['H2', 'GUADALUPE3', 'Guadalupe Iii', [], ['GUADALUPE31', 'GUADALUPE32', 'GUADALUPE33', 'GUADALUPE34', 'GUADALUPE35', 'GUADALUPE36']], ['H3', 'GUADALUPE4', 'Guadalupe Iv', [], ['GUADALUPE41', 'GUADALUPE42', 'GUADALUPE43']], ['H4', 'PORCEII', 'Porce Iii', ['PORCE2'], ['PORCEII1', 'PORCEII2', 'PORCEII3']], ['H5', 'PORCEIII', 'Porce Iii', ['PORCE3'], ['PORCEIII4', 'PORCEIII3', 'PORCEIII2', 'PORCEIII1']], ['H6', 'GUATAPE', 'Guatape', ['GUATAPE'], ['GUATAPE1', 'GUATAPE2', 'GUATAPE3', 'GUATAPE4', 'GUATAPE5', 'GUATAPE6', 'GUATAPE7', 'GUATAPE8']], ['H7', 'RIOABAJO', 'Rio Abajo', [], []], ['H8', 'SONSON', 'Sonson', ['MSONSON'], ['SONSON']], ['H9', 'RIOFRIO(TAMESIS)', 'Tamesis', ['MRFRIOTAMES', 'MRIOFRIO1', 'RIOFRIO2'], ['RIOFRIO\\(TAMESIS\\)']], ['H10', 'AYURA', 'Ayura', ['M_AYURA'], ['AYURA']], ['H11', 'NIQUIA', 'Niquia', ['MNIQUIA'], ['NIQUIA']], ['H12', 'URRA1', 'Urra I', ['URRA'], ['URRA1']], ['H13', 'CHIVOR', 'Chivor', ['CHIVOR'], ['CHIVOR1', 'CHIVOR2', 'CHIVOR3', 'CHIVOR4', 'CHIVOR5', 'CHIVOR6', 'CHIVOR7', 'CHIVOR8']], ['H14', 'CUCUANA', 'Cucuana', ['CUCUANA'], ['CUCUANA1', 'CUCUANA2']], ['H15', 'CALIMA', 'Calima', ['CALIMA1'], ['CALIMA1', 'CALIMA2', 'CALIMA3', 'CALIMA4']]]
 CentralesOrder = ['E1','T1','T2','T3','T4','T12','T6','T7','T8','T9','T10','T11','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12','H13','H14','H15','H16','H17','H18','H19','H20','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12','H13','H14','H15'];
 
 Centrales = []
 
 # Initialize
 def initialize():
-    for c in CentralesRaw:
-        temp = Central(c[0], c[1], c[2], c[3])
+    for c in range(0,len(CentralesRaw)):
+        temp = Central(CentralesRaw[c][0], CentralesRaw[c][1], CentralesRaw[c][2], CentralesRaw[c][3], CentralesRaw[c][4], c)
         Centrales.append(temp)
 
 # Add multiple lists
