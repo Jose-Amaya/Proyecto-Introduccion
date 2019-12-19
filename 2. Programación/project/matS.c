f0f38c040739c01e23d28a62fd691934a862de96
JOSE AMAYA <jose2170484@correo.uis.edu.co>
2019-12-19 14:18:47 -0500

    Upgrade

--- 2. Programación/project/matS.c
+++ 2. Programación/project/matS.c
@@ -19,7 +19,7 @@
 #define BCM2708_PERI_BASE        0x3F000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
 
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -27,16 +27,17 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <sched.h>
 
 
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
 
 int  mem_fd;
-void *gpio_map;
+void* gpio_map;
 
 // I/O access
-volatile unsigned *gpio;
+volatile unsigned* gpio;
 
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
@@ -58,87 +59,88 @@ void setup_io();
 
 //#define deployed
 
-const uint8_t r[8] = {17, 4, 3, 2, 26, 19, 13, 6}; // GPIO rows
-const uint8_t c[8] = {24, 25, 8, 7, 12, 16, 20, 21}; // GPIO columns
+const uint8_t r[8] = { 17, 4, 3, 2, 26, 19, 13, 6 }; // GPIO rows
+const uint8_t c[8] = { 24, 25, 8, 7, 12, 16, 20, 21 }; // GPIO columns
 
 uint32_t rMask;
 uint32_t cMask;
 
-const uint32_t waitTime = (1000000/120)/8/10;
-const uint32_t lastR[8] = {7,0,1,2,3,4,5,6};
+const uint32_t waitTime = (1000000 / 120) / 8 / 10;
+const uint32_t lastR[8] = { 7,0,1,2,3,4,5,6 };
 
 volatile uint8_t matrix[8][7] = {};
 
 
-void allOff(){
+void allOff() {
 	GPIO_CLR = rMask;
-	
-	#ifdef deployed
+
+#ifdef deployed
 	GPIO_CLR = cMask;
-	#else
+#else
 	GPIO_SET = cMask;
-	#endif
+#endif
 }
 
-void columnsOff(){
-   #ifdef deployed
+void columnsOff() {
+#ifdef deployed
 	GPIO_CLR = cMask;
-	#else
+#else
 	GPIO_SET = cMask;
-	#endif
+#endif
 }
 
-void updateLeds(){
+void updateLeds() {
 	allOff();
-	for(size_t i = 0; i < 8; i++){
+	for (size_t i = 0; i < 8; i++) {
 		GPIO_CLR = 1 << r[lastR[i]];
 		columnsOff();
-		
+
 		uint32_t mask = matrix[i][0] << c[0] | matrix[i][1] << c[1] | matrix[i][2] << c[2] | matrix[i][3] << c[3] | matrix[i][4] << c[4] | matrix[i][5] << c[5] | matrix[i][6] << c[6];
-		
+
 		GPIO_SET = 1 << r[i]; // Turn on the row
-		
+
 		// Turn on the columns
-		#ifdef deployed
-		GPIO_SET = mask; 
-		#else
+#ifdef deployed
+		GPIO_SET = mask;
+#else
 		GPIO_CLR = mask;
-		#endif
-		
+#endif
+
 		usleep(waitTime);
 	}
 }
 
-void *updateData(void *ptr){
-   while(1){
-		FILE * matrixData;
-		
+void* updateData(void* ptr) {
+
+	while (1) {
+		FILE* matrixData;
+
 		matrixData = fopen("./data/files/matSdata.bin", "r");
-		
+
 		uint8_t buffer[56];
 		int dataRead = fread(buffer, 1, 56, matrixData);
 		fclose(matrixData);
-		
-		if(dataRead == 56){
-			
-			for(size_t i = 0; i <8; i++){
-				for(size_t j = 0; j < 7; j++){
-					matrix[i][j] = buffer[j+i*7] & 1;
+
+		if (dataRead == 56) {
+
+			for (size_t i = 0; i < 8; i++) {
+				for (size_t j = 0; j < 7; j++) {
+					matrix[i][j] = buffer[j + i * 7] & 1;
 				}
 			}
-			
-         #ifdef deployed
-            matrix[3][0] = matrix[1][1]; // Arreglo
-            matrix[2][6] = matrix[1][3]; // Arreglo
-         #endif
-			
+
+#ifdef deployed
+			matrix[3][0] = matrix[1][1]; // Arreglo
+			matrix[2][6] = matrix[1][3]; // Arreglo
+#endif
+
 		}
-		
+
 		sleep(1);
-   }
+	}
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	// Set up gpi pointer for direct register access
 	setup_io();
@@ -147,7 +149,7 @@ int main(int argc, char **argv)
 	cMask = 1 << c[0] | 1 << c[1] | 1 << c[2] | 1 << c[3] | 1 << c[4] | 1 << c[5] | 1 << c[6] | 1 << c[7];
 
 	// Set rows and columns to output
-	for(size_t i = 0; i < 8; i++){
+	for (size_t i = 0; i < 8; i++) {
 		INP_GPIO(r[i]);
 		OUT_GPIO(r[i]);
 		INP_GPIO(c[i]);
@@ -156,10 +158,22 @@ int main(int argc, char **argv)
 
 	allOff();
 
-    pthread_t updateThread;
-    pthread_create(&updateThread, NULL, updateData, NULL);
-	
-	while(1){
+	pthread_t updateThread;
+	pthread_create(&updateThread, NULL, updateData, NULL);
+
+	// Set priorities to avoid flickering
+	//nice(-20); // niceness to highest priority // Only with SCHED_OTHER or SCHED_BATCH
+
+	struct sched_param sp = { .sched_priority = 99 };
+	sched_setscheduler(0, SCHED_RR, &sp); // scheduler to RR, and highest priority
+
+	cpu_set_t* set;
+	set = CPU_ALLOC(1);
+	CPU_SET(0, set);
+	sched_setaffinity(0, sizeof(set), set); // run only on the cpu 0, which is only running this process
+	CPU_FREE(set);
+
+	while (1) {
 		updateLeds();
 	}
 
@@ -173,31 +187,31 @@ int main(int argc, char **argv)
 //
 void setup_io()
 {
-   /* open /dev/mem */
-   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("can't open /dev/mem \n");
-      exit(-1);
-   }
+	/* open /dev/mem */
+	if ((mem_fd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) {
+		printf("can't open /dev/mem \n");
+		exit(-1);
+	}
 
-   /* mmap GPIO */
-   gpio_map = mmap(
-      NULL,             //Any adddress in our space will do
-      BLOCK_SIZE,       //Map length
-      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-      MAP_SHARED,       //Shared with other processes
-      mem_fd,           //File to map
-      GPIO_BASE         //Offset to GPIO peripheral
-   );
+	/* mmap GPIO */
+	gpio_map = mmap(
+		NULL,             //Any adddress in our space will do
+		BLOCK_SIZE,       //Map length
+		PROT_READ | PROT_WRITE,// Enable reading & writting to mapped memory
+		MAP_SHARED,       //Shared with other processes
+		mem_fd,           //File to map
+		GPIO_BASE         //Offset to GPIO peripheral
+	);
 
-   close(mem_fd); //No need to keep mem_fd open after mmap
+	close(mem_fd); //No need to keep mem_fd open after mmap
 
-   if (gpio_map == MAP_FAILED) {
-      printf("mmap error %d\n", (int)gpio_map);//errno also set!
-      exit(-1);
-   }
+	if (gpio_map == MAP_FAILED) {
+		printf("mmap error %d\n", (int)gpio_map);//errno also set!
+		exit(-1);
+	}
 
-   // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
+	// Always use volatile pointer!
+	gpio = (volatile unsigned*)gpio_map;
 
 
 } // setup_io
